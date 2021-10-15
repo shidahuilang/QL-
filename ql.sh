0bd63ff7a0bdf91ebf6158f34ef71e8beb473289
shidahuilang <81285256+shidahuilang@users.noreply.github.com>
2021-10-15 19:11:51 +0800

    Update ql.sh

--- ql.sh
+++ ql.sh
@@ -293,7 +293,7 @@ if [[ `docker ps -a | grep -c "whyour"` -ge '1' ]]; then
 			TIME y "开始安装脚本，请耐心等待..."
 			echo
 			cp -r /opt/qlbak/db/env.db /opt/ql/db
-			cp -r /opt/qlbak/config /opt/ql/config
+			cp -r /opt/qlbak/config /opt/ql
 			docker exec -it qinglong bash -c  "$(curl -fsSL https://ghproxy.com/https://raw.githubusercontent.com/shidahuilang/QL-/main/feverrun.sh)"
 			if [[ $? -ne 0 ]];then
 				docker exec -it qinglong bash -c "$(curl -fsSL https://cdn.jsdelivr.net/gh/shidahuilang/QL-@main/feverrun.sh)"
