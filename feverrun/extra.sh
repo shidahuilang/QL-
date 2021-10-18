a33f454bc1f5446cbee6e31aea47ea84a7a7162a
shidahuilang <81285256+shidahuilang@users.noreply.github.com>
2021-10-18 20:47:41 +0800

    Update extra.sh

--- feverrun/extra.sh
+++ feverrun/extra.sh
@@ -5,8 +5,6 @@
 ## 安装python依赖使用 pip3 install xxx
 
 ql raw https://raw.githubusercontent.com/shidahuilang/QL-/main/feverrun/Disable.py
-ql raw https://raw.githubusercontent.com/shidahuilang/QL-/main/wx_jysz/crypto-js.js
-ql raw https://raw.githubusercontent.com/shidahuilang/QL-/main/wx_jysz/wx_jysz.js
 ql repo https://ghproxy.com/https://github.com/shufflewzc/faker2.git "jd_|jx_|gua_|jddj_|getJDCookie" "activity|backUp" "^jd[^_]|USER|function|utils|ZooFaker_Necklace.js|JDJRValidator_Pure|sign_graphics_validate|ql"
 ql repo https://github.com/feverrun/my_scripts.git "jd_|jx_|gua|jddj|getCookie|getJDCookie" "backUp" "^(jd|JD|JS)[^_]|USER|sendNotify|utils"
 task raw_feverrun_Disable.py
