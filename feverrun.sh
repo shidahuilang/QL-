7cb81ff21edd80401b4803ecc84e59d994faf82d
shidahuilang <81285256+shidahuilang@users.noreply.github.com>
2021-10-19 01:49:00 +0800

    Update feverrun.sh

--- feverrun.sh
+++ feverrun.sh
@@ -37,9 +37,9 @@ wx_jysz_shell_path=$dir_script/wx_jysz.js
 OpenCard_shell_path=$dir_script/raw_jd_OpenCard.py
 task_before_shell_path=$dir_shell/task_before.sh
 sample_shell_path=/ql/sample/config.sample.sh
-git clone https://ghproxy.com/https://github.com/shidahuilang/QL- qlwj
+git clone https://ghproxy.com/https://github.com/281677160/ql qlwj
 if [[ $? -ne 0 ]];then
-	mkdir -p /QL-/qlwj
+	mkdir -p /ql/qlwj
 	curl -fsSL https://cdn.jsdelivr.net/gh/shidahuilang/QL-@main/feverrun/wx_jysz.js > /ql/qlwj/wx_jysz.js
 	curl -fsSL https://cdn.jsdelivr.net/gh/shidahuilang/QL-@main/feverrun/crypto-js.js > /ql/qlwj/crypto-js.js
 	curl -fsSL https://cdn.jsdelivr.net/gh/shidahuilang/QL-@main/feverrun/config.sample.sh > /ql/qlwj/config.sample.sh
@@ -53,19 +53,65 @@ if [[ $? -ne 0 ]];then
 fi
 
 # 授权
-chmod -R +x /QL-/qlwj
+chmod -R +x /ql/qlwj
 
-cp -Rf /QL-/qlwj/feverrun/config.sample.sh /ql/config/config.sh
-cp -Rf /QL-/qlwj/feverrun/config.sample.sh /ql/sample/config.sample.sh
-cp -Rf /QL-/qlwj/feverrun/extra.sh /ql/config/extra.sh
-cp -Rf /QL-/qlwj/feverrun/extra.sh /ql/sample/extra.sample.sh
-cp -Rf /QL-/qlwj/feverrun/raw_jd_OpenCard.py /ql/scripts/raw_jd_OpenCard.py
-cp -Rf /QL-/qlwj/feverrun/wskey.py /ql/scripts/wskey.py
-cp -Rf /QL-/qlwj/feverrun/wx_jysz.js /ql/scripts/wx_jysz.js
-cp -Rf /QL-/qlwj/feverrun/crypto-js.js /ql/scripts/crypto-js.js
+cp -Rf /ql/qlwj/feverrun/config.sample.sh /ql/config/config.sh
+cp -Rf /ql/qlwj/feverrun/config.sample.sh /ql/sample/config.sample.sh
+cp -Rf /ql/qlwj/feverrun/extra.sh /ql/config/extra.sh
+cp -Rf /ql/qlwj/feverrun/extra.sh /ql/sample/extra.sample.sh
+cp -Rf /ql/qlwj/feverrun/raw_jd_OpenCard.py /ql/scripts/raw_jd_OpenCard.py
+cp -Rf /ql/qlwj/feverrun/wskey.py /ql/scripts/wskey.py
+cp -Rf /ql/qlwj/feverrun/wx_jysz.js /ql/scripts/wx_jysz.js
+cp -Rf /ql/qlwj/feverrun/crypto-js.js /ql/scripts/crypto-js.js
 echo
 echo
-
+TIME g "正在安装依赖，安装依赖需要时间，请耐心等候..."
+echo
+echo
+npm config set registry https://registry.npm.taobao.org
+cd /ql
+npm install -g npm
+cd /ql
+npm install -g png-js
+cd /ql
+npm install -g date-fns
+cd /ql
+npm install -g axios
+cd /ql
+npm install -g crypto-js
+cd /ql
+npm install -g ts-md5
+cd /ql
+npm install -g tslib
+cd /ql
+npm install -g @types/node
+cd /ql
+npm install -g requests
+cd /ql
+npm install -g tough-cookie
+cd /ql
+npm install -g jsdom
+cd /ql
+npm install -g download
+cd /ql
+npm install -g tunnel
+cd /ql
+npm install -g fs
+cd /ql
+npm install -g ws
+cd /ql
+pip3 install requests
+cd /ql
+cd /ql/scripts/ && apk add --no-cache build-base g++ cairo-dev pango-dev giflib-dev && npm i && npm i -S ts-node typescript @types/node date-fns axios png-js canvas --build-from-source
+cd /ql
+apk add --no-cache build-base g++ cairo-dev pango-dev giflib-dev && cd scripts && npm install canvas --build-from-source
+cd /ql
+apk add python3 zlib-dev gcc jpeg-dev python3-dev musl-dev freetype-dev
+cd /ql
+echo
+TIME g "依赖安装完毕..."
+echo
+echo
 # 将 extra.sh 添加到定时任务
 if [ "$(grep -c extra /ql/config/crontab.list)" = 0 ]; then
     echo
@@ -81,12 +127,12 @@ fi
 if [ "$(grep -c wskey.py /ql/config/crontab.list)" = 0 ]; then
     echo
     echo
-    TIME g "开始添加 [WSKEY更新自动转换]"
+    TIME g "开始添加 [早9晚11点WSKEY转换]"
     echo
     echo
     # 获取token
     token=$(cat /ql/config/auth.json | jq --raw-output .token)
-    curl -s -H 'Accept: application/json' -H "Authorization: Bearer $token" -H 'Content-Type: application/json;charset=UTF-8' -H 'Accept-Language: zh-CN,zh;q=0.9' --data-binary '{"name":"WSKEY更新自动转换","command":"task wskey.py","schedule":"* */8 * * *"}' --compressed 'http://127.0.0.1:5700/api/crons?t=1633428022377'
+    curl -s -H 'Accept: application/json' -H "Authorization: Bearer $token" -H 'Content-Type: application/json;charset=UTF-8' -H 'Accept-Language: zh-CN,zh;q=0.9' --data-binary '{"name":"早9晚11点更新","command":"task wskey.py","schedule":"0 9,23 * * *"}' --compressed 'http://127.0.0.1:5700/api/crons?t=1633428022377'
 fi
 
 # 将 bot 添加到定时任务
@@ -141,69 +187,28 @@ if [[ "$(grep -c JD_WSCK=\"pin= /ql/config/env.sh)" = 1 ]]; then
 	echo
     fi
 fi
+echo
+echo
+TIME g "拉取自动助力脚本"
+echo
+echo
+rm -fr /ql/azcg.log
+ql extra |tee azcg.log
+rm -rf /ql/qlwj
+echo
+echo
 if [[ "$(grep -c JD_WSCK=\"pin= /ql/config/env.sh)" = 0 ]] && [[ "$(grep -c JD_COOKIE=\"pt_key= /ql/config/env.sh)" = 0 ]]; then
     TIME y "没发现WSKEY或者PT_KEY，请注意设置好KEY，要不然脚本不会运行!"
 fi
 echo
 echo
-TIME g "拉取自动助力脚本任务"
-echo
-echo
-rm -fr /QL-/azcg.log
-ql extra |tee azcg.log
-rm -rf /QL-/qlwj
-
-echo
-if [[ `ls -a |grep -c "添加成功" /QL-/azcg.log` -ge '1' ]] && [[ `ls -a |grep -c "执行结束" /QL-/azcg.log` -ge '1' ]] || [[ `ls -a |grep -c "开始更新仓库" /QL-/azcg.log` -ge '1' ]]; then
+if [[ `ls -a |grep -c "添加成功" /ql/azcg.log` -ge '1' ]] && [[ `ls -a |grep -c "执行结束" /ql/azcg.log` -ge '1' ]] || [[ `ls -a |grep -c "开始更新仓库" /ql/azcg.log` -ge '1' ]]; then
 	TIME g "脚本安装完成!"
-	rm -fr /QL-/azcg.log
-echo
-echo
-TIME g "正在安装依赖，安装依赖需要时间，请耐心等候..."
-echo
-echo
-npm config set registry https://registry.npm.taobao.org
-cd /ql
-npm install -g npm
-cd /ql
-npm install -g png-js
-cd /ql
-npm install -g date-fns
-cd /ql
-npm install -g axios
-cd /ql
-npm install -g crypto-js
-cd /ql
-npm install -g ts-md5
-cd /ql
-npm install -g tslib
-cd /ql
-npm install -g @types/node
-cd /ql
-npm install -g requests
-cd /ql
-npm install -g tough-cookie
-cd /ql
-npm install -g jsdom
-cd /ql
-npm install -g download
-cd /ql
-npm install -g tunnel
-cd /ql
-npm install -g fs
-cd /ql
-npm install -g ws
-cd /ql
-pip3 install requests
-cd /ql
-cd /ql/scripts/ && apk add --no-cache build-base g++ cairo-dev pango-dev giflib-dev && npm i && npm i -S ts-node typescript @types/node date-fns axios png-js canvas --build-from-source
-cd /ql
-apk add --no-cache build-base g++ cairo-dev pango-dev giflib-dev && cd scripts && npm install canvas --build-from-source
-cd /ql
-apk add python3 zlib-dev gcc jpeg-dev python3-dev musl-dev freetype-dev
-cd /ql
-echo
-TIME g "依赖安装完毕..."
-echo
+	rm -fr /ql/azcg.log
+else
+	TIME r "脚本安装失败，请用一键单独安装任务重新尝试!"
+	rm -fr /ql/azcg.log
+	exit 1
+fi
 echo
 exit 0
