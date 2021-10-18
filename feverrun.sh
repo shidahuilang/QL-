70b305013bb75972f0d4ae10d4a6982ed03a76c9
shidahuilang <81285256+shidahuilang@users.noreply.github.com>
2021-10-19 01:52:59 +0800

    Update feverrun.sh

--- feverrun.sh
+++ feverrun.sh
@@ -64,54 +64,7 @@ cp -Rf /ql/qlwj/feverrun/wskey.py /ql/scripts/wskey.py
 cp -Rf /ql/qlwj/feverrun/wx_jysz.js /ql/scripts/wx_jysz.js
 cp -Rf /ql/qlwj/feverrun/crypto-js.js /ql/scripts/crypto-js.js
 echo
-echo
-TIME g "正在安装依赖，安装依赖需要时间，请耐心等候..."
-echo
-echo
-npm config set registry https://registry.npm.taobao.org
-cd /ql
-npm install -g npm
-cd /ql
-npm install -g png-js
-cd /ql
-npm install -g date-fns
-cd /ql
-npm install -g axios
-cd /ql
-npm install -g crypto-js
-cd /ql
-npm install -g ts-md5
-cd /ql
-npm install -g tslib
-cd /ql
-npm install -g @types/node
-cd /ql
-npm install -g requests
-cd /ql
-npm install -g tough-cookie
-cd /ql
-npm install -g jsdom
-cd /ql
-npm install -g download
-cd /ql
-npm install -g tunnel
-cd /ql
-npm install -g fs
-cd /ql
-npm install -g ws
-cd /ql
-pip3 install requests
-cd /ql
-cd /ql/scripts/ && apk add --no-cache build-base g++ cairo-dev pango-dev giflib-dev && npm i && npm i -S ts-node typescript @types/node date-fns axios png-js canvas --build-from-source
-cd /ql
-apk add --no-cache build-base g++ cairo-dev pango-dev giflib-dev && cd scripts && npm install canvas --build-from-source
-cd /ql
-apk add python3 zlib-dev gcc jpeg-dev python3-dev musl-dev freetype-dev
-cd /ql
-echo
-TIME g "依赖安装完毕..."
-echo
-echo
+
 # 将 extra.sh 添加到定时任务
 if [ "$(grep -c extra /ql/config/crontab.list)" = 0 ]; then
     echo
