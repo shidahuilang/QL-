e5becbeaaa813926907b3994c77dac9046e92f49
shidahuilang <81285256+shidahuilang@users.noreply.github.com>
2021-11-02 17:29:45 +0800

    Update extra.sh

--- feverrun/extra.sh
+++ feverrun/extra.sh
@@ -7,4 +7,4 @@
 ql raw https://raw.githubusercontent.com/shidahuilang/QL-/main/feverrun/Disable.py
 ql repo https://ghproxy.com/https://github.com/shufflewzc/faker2.git "jd_|jx_|gua_|jddj_|getJDCookie" "activity|backUp" "^jd[^_]|USER|function|utils|ZooFaker_Necklace.js|JDJRValidator_Pure|sign_graphics_validate|ql"
 ql repo https://github.com/feverrun/my_scripts.git "jd_|jx_|gua|jddj|getCookie|getJDCookie" "backUp" "^(jd|JD|JS)[^_]|USER|sendNotify|utils"
-task shufflewzc_faker2_jd_disable.py
+task feverrun_Disable.py
