be2daacb6e43705ed10f8e93733d46d01cb71800
shidahuilang <81285256+shidahuilang@users.noreply.github.com>
2021-11-21 04:15:08 +0800

    Update nvjdc.sh

--- nvjdc.sh
+++ nvjdc.sh
@@ -213,7 +213,7 @@ fi
 
 #拉取nvjdc镜像
 log_action_begin_msg "开始拉取nvjdc镜像文件，nvjdc镜像比较大，请耐心等待"
-docker pull 10529459/lanyannvjdc:1.4
+docker pull registry.cn-hangzhou.aliyuncs.com/nvjdc/langnvjdc:1.4
 log_action_end_msg $?
 
 #创建并启动nvjdc容器
