a5cc7d811ca9cbfab541efffc046008df08220da
shidahuilang <81285256+shidahuilang@users.noreply.github.com>
2021-11-21 02:17:28 +0800

    Update nvjdc.sh

--- nvjdc.sh
+++ nvjdc.sh
@@ -218,7 +218,7 @@ log_action_end_msg $?
 
 #创建并启动nvjdc容器
 log_action_begin_msg "开始创建nvjdc容器"
-docker run   --name nvjdc -p ${jdcport}:80 -d  -v  "$(pwd)"/Config.json:/app/Config/Config.json:ro \
+docker run   --name lanyannvjdc -p ${jdcport}:80 -d  -v  "$(pwd)"/Config.json:/app/Config/Config.json:ro \
 -v "$(pwd)"/.local-chromium:/app/.local-chromium  \
 -it --privileged=true  10529459/lanyannvjdc:1.4
 
@@ -230,24 +230,24 @@ echo -e "${green}Faker集合仓库频道：${plain}${red}https://t.me/pandaqx${p
 }
 
 update_nvjdc(){
-  cd /root/nvjdc
-portinfo=$(docker port nvjdc | head -1  | sed 's/ //g' | sed 's/80\/tcp->0.0.0.0://g')
+  cd /root/lanyannvjdc
+portinfo=$(docker port lanyannvjdc | head -1  | sed 's/ //g' | sed 's/80\/tcp->0.0.0.0://g')
 baseip=$(curl -s ipip.ooo)  > /dev/null
-docker rm -f nvjdc
-docker pull nolanhzy/nvjdc:latest
-docker run   --name nvjdc -p ${portinfo}:80 -d  -v  "$(pwd)"/Config.json:/app/Config/Config.json:ro \
+docker rm -f lanyannvjdc
+docker pull 10529459/lanyannvjdc:1.4
+docker run   --name lanyannvjdc -p ${portinfo}:80 -d  -v  "$(pwd)"/Config.json:/app/Config/Config.json:ro \
 -v "$(pwd)"/.local-chromium:/app/.local-chromium  \
--it --privileged=true  nolanhzy/nvjdc:latest
-echo -e "${green}nvjdc更新完毕，脚本自动退出。${plain}"
+-it --privileged=true  10529459/lanyannvjdc:1.4
+echo -e "${green}lanyannvjdc更新完毕，脚本自动退出。${plain}"
 echo -e "${green}面板访问地址：http://${baseip}:${portinfo}${plain}"
 exit 0
 }
 
 uninstall_nvjdc(){
-docker rm -f nvjdc
-docker rmi -f nolanhzy/nvjdc:0.4
-rm -rf nvjdc
-echo -e "${green}nvjdc面板已卸载，脚本自动退出。${plain}"
+docker rm -f lanyannvjdc
+docker rmi -f 10529459/lanyannvjdc:1.4
+rm -rf lanyannvjdc
+echo -e "${green}lanyannvjdc面板已卸载，脚本自动退出。${plain}"
 exit 0
 }
 
