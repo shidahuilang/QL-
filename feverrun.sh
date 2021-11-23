99397b8e2165615066be2cf90b9b69570094fda8
shidahuilang <81285256+shidahuilang@users.noreply.github.com>
2021-11-24 00:18:28 +0800

    Update feverrun.sh

--- feverrun.sh
+++ feverrun.sh
@@ -154,6 +154,17 @@ if [ "$(grep -c raw_jd_OpenCard.py /ql/config/crontab.list)" = 0 ]; then
 fi
 sleep 2
 echo
+# 将 jd_get_share_code.js 添加到定时任务
+if [ "$(grep -c jd_get_share_code.js /ql/config/crontab.list)" = 0 ]; then
+    echo
+    TIME g "添加任务 [获取助力码]"
+    echo
+    # 获取token
+    token=$(cat /ql/config/auth.json | jq --raw-output .token)
+    curl -s -H 'Accept: application/json' -H "Authorization: Bearer $token" -H 'Content-Type: application/json;charset=UTF-8' -H 'Accept-Language: zh-CN,zh;q=0.9' --data-binary '{"name":"获取助力码","command":"task jd_get_share_code.js","schedule":"20 13 * * 6"}' --compressed 'http://127.0.0.1:5700/api/crons?t=1637505495835'
+fi
+sleep 2
+echo
 # 将 jd_Evaluation.py 添加到定时任务
 if [ "$(grep -c jd_Evaluation.py /ql/config/crontab.list)" = 0 ]; then
     echo
