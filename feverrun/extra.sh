3c1002a0fa4105fd3318001df3b0d5d314019dbd
shidahuilang <81285256+shidahuilang@users.noreply.github.com>
2021-11-26 01:05:06 +0800

    Update extra.sh

--- feverrun/extra.sh
+++ feverrun/extra.sh
@@ -5,5 +5,5 @@
 ## 安装python依赖使用 pip3 install xxx
 ql raw https://raw.githubusercontent.com/shidahuilang/QL-/main/feverrun/disableDuplicateTasksImplement.py
 ql repo https://ghproxy.com/https://github.com/mmnvnmm/omo.git "card|tools"
-ql repo https://ghproxy.com/https://github.com/feverrun/my_scripts.git "jd_|jx_|gua|jddj|getCookie|getJDCookie" "backUp" "^(jd|JD|JS)[^_]|USER|sendNotify|utils"
+ql repo https://ghproxy.com/https://github.com/feverrun/my_scripts.git "jd_|jx_|jddj|getCookie|getJDCookie" "backUp" "^(jd|JD|JS)[^_]|USER|sendNotify|utils"
 task raw_feverrun_disableDuplicateTasksImplement.py	
