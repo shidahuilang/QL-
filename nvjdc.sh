3f63bb37ebcb358a3266a5fbb9ccb156dfd6e2f4
shidahuilang <81285256+shidahuilang@users.noreply.github.com>
2021-11-27 23:54:59 +0800

    Update nvjdc.sh

--- nvjdc.sh
+++ nvjdc.sh
@@ -131,12 +131,6 @@ copyright(){
 echo -e "
 —————————————————————————————————————————————————————————————
         Nvjdc自助面板一键安装脚本               
- ${green}                
-        
-
-        
-
-        
 —————————————————————————————————————————————————————————————
 "
 }
@@ -146,9 +140,11 @@ exit
 
 install_nvjdc(){
 echo -e "${red}开始进行安装,请根据命令提示操作${plain}"
+echo -e "检测到已有nvjdc面板，正在删除旧的nvjdc文件，请稍后..."
+rm  -rf /root/nvjdc
 git clone https://github.com/btlanyan/nvjdc.git /root/nvjdc
 cd /root/nvjdc && mkdir -p  .local-chromium/Linux-884014 && cd .local-chromium/Linux-884014
-echo -e "下载并解压，请耐心等待"
+echo -e "${red}下载并解压,请耐心等待${plain}"
 wget https://mirrors.huaweicloud.com/chromium-browser-snapshots/Linux_x64/884014/chrome-linux.zip && unzip chrome-linux.zip
 rm  -f chrome-linux.zip > /dev/null 2>&1 
 rm  -f /root/nvjdc/Config/Config.json > /dev/null 2>&1
@@ -195,13 +191,14 @@ echo -e "检测到系统未安装docker，开始安装docker"
     curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && chmod +x /usr/local/bin/docker-compose && ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 fi
 cp -r /root/nvjdc/Config.json /root/nvjdc/Config/Config.json
+rm  -f /root/nvjdc/Config.json
 #拉取nvjdc镜像
 echo -e "开始拉取nvjdc镜像文件，nvjdc镜像比较大，请耐心等待"
 docker pull shidahuilang/nvjdc:1.4
 echo
 cd  /root/nvjdc
 echo -e "创建并启动nvjdc容器"
-sudo docker run   --name nolanjdc -p ${jdcport}:80 -d  -v  "$(pwd)":/app \
+sudo docker run   --name nvjdc -p ${jdcport}:80 -d  -v  "$(pwd)":/app \
 -v /etc/localtime:/etc/localtime:ro \
 -it --privileged=true  shidahuilang/nvjdc:1.4
 
@@ -217,7 +214,7 @@ portinfo=$(docker port nvjdc | head -1  | sed 's/ //g' | sed 's/80\/tcp->0.0.0.0
 baseip=$(curl -s ipip.ooo)  > /dev/null
 docker rm -f nvjdc
 docker pull shidahuilang/nvjdc:1.4
-sudo docker run   --name nolanjdc -p ${jdcport}:80 -d  -v  "$(pwd)":/app \
+sudo docker run   --name nvjdc -p ${jdcport}:80 -d  -v  "$(pwd)":/app \
 -v /etc/localtime:/etc/localtime:ro \
 -it --privileged=true  shidahuilang/nvjdc:1.4
 echo -e "${green}nvjdc更新完毕，脚本自动退出。${plain}"
