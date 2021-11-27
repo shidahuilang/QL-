b4914b918b441016eb2fc7c213cc350b67254d0c
shidahuilang <81285256+shidahuilang@users.noreply.github.com>
2021-11-27 22:04:10 +0800

    Update nvjdc.sh

--- nvjdc.sh
+++ nvjdc.sh
@@ -165,7 +165,6 @@ echo -e "${red}开始进行安装,请根据命令提示操作${plain}"
 git clone https://github.com/btlanyan/nvjdc.git /root/nvjdc
 cd /root/nvjdc && mkdir -p  .local-chromium/Linux-884014 && cd .local-chromium/Linux-884014
 wget https://mirrors.huaweicloud.com/chromium-browser-snapshots/Linux_x64/884014/chrome-linux.zip && unzip chrome-linux.zip > /dev/null 2>&1 
-#unzip chrome-linux.zip > /dev/null 2>&1 
 rm  -f chrome-linux.zip > /dev/null 2>&1 
 rm  -f /root/nvjdc/Config/Config.json
 cd .. && cd ..
