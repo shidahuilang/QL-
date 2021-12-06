645b92a856481d981f0dedfb17ca2f60614b19fd
shidahuilang <81285256+shidahuilang@users.noreply.github.com>
2021-12-06 16:17:46 +0800

    Update vip2.sh

--- feverrun/vip2.sh
+++ feverrun/vip2.sh
@@ -231,6 +231,7 @@ docker run -dit \
   -v $QL_PATH/ql/jbot:/ql/jbot \
   -v $QL_PATH/ql/raw:/ql/raw \
   -v $QL_PATH/ql/jd:/ql/jd \
+  -v $QL_PATH/ql/jd:/ql/deps \
   -v $QL_PATH/ql/repo:/ql/repo \
   ${NETWORK} \
   --name qinglong \
