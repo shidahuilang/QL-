1787e7099fb25e0defad9ff9c4bc9f255e035e9f
shidahuilang <81285256+shidahuilang@users.noreply.github.com>
2021-12-11 01:52:14 +0800

    Update extra.sh

--- feverrun/extra.sh
+++ feverrun/extra.sh
@@ -5,11 +5,11 @@
 ## 安装python依赖使用 pip3 install xxx
 #京东到家果园任务
 ql raw https://raw.githubusercontent.com/passerby-b/JDDJ/main/jddj_fruit.js
-ql rmlog 7
 #超级直播间红包雨
 #ql raw https://raw.githubusercontent.com/KingRan/JDJB/main/jd_live_redrain.js
 #宠汪汪有就换
 ql repo https://github.com/ccwav/QLScript2.git "jd_joy_reward_Mod" "NoUsed" "ql|jdCookie|JS_USER_AGENTS|sendNotify|USER_AGENTS|utils"
 #feverrun库
 ql repo https://ghproxy.com/https://github.com/feverrun/my_scripts.git "jd_|jx_|jddj|getCookie|getJDCookie" "backUp" "^(jd|JD|JS)[^_]|USER|sendNotify|utils"
-#task disableDuplicateTasksImplement.py
+ql repo https://github.com/JDHelloWorld/jd_scripts.git "jd_fruit|jd_plantBean|jd_jdfactory|jd_dreamFactory|jd_pet|jd_health|jd_sgmh|jd_jxmc|jd_cfd|jd_88hb|jd_api_test" "activity|backUp|Coupon|enen|update|collect|game|shell|stock|getCoin|moreTask|jd_jxmc.js" "^jd[^_]|^USER|^TS|^JS|jdCookie|sendNotify|tools|utils|package|jd_validate_Worker"
+task disableDuplicateTasksImplement.py
