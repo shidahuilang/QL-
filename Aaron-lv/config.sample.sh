a28fecb53a34331900d7dafa6a93413625a21be7
shidahuilang <81285256+shidahuilang@users.noreply.github.com>
2021-12-11 23:09:30 +0800

    Update config.sample.sh

--- Aaron-lv/config.sample.sh
+++ Aaron-lv/config.sample.sh
@@ -91,7 +91,7 @@ export PUSH_PLUS_TOKEN=""
 ## 下方填写您的一对多推送的 "群组编码" ，（一对多推送下面->您的群组(如无则新建)->群组编码）
 ## 1. 需订阅者扫描二维码 2、如果您是创建群组所属人，也需点击“查看二维码”扫描绑定，否则不能接受群组消息推送
 export PUSH_PLUS_USER=""
-
+export jd_work_price="1"
 ## 9. go-cqhttp
 ## gobot_url 推送到个人QQ: http://127.0.0.1/send_private_msg  群：http://127.0.0.1/send_group_msg 
 ## gobot_token 填写在go-cqhttp文件设置的访问密钥
