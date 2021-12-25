b1974aedf7e2d60b25e6d32edd4e3b642abb4fa1
shidahuilang <81285256+shidahuilang@users.noreply.github.com>
2021-12-25 17:57:28 +0800

    Update disableDuplicateTasksImplement.py

--- Aaron-lv/disableDuplicateTasksImplement.py
+++ Aaron-lv/disableDuplicateTasksImplement.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 # 环境变量
 #1️⃣RES_SUB
@@ -6,6 +5,10 @@
 #说明
 #1️⃣需保留的脚本前缀列表（不管是否重复），用 & 分割，默认保留 shufflewzc_faker2&JDHelloWorld_jd_scripts
 #2️⃣保留后是否禁用重复的带其他前缀脚本，默认禁用
+
+#示例
+#1️⃣Aaron-lv_sync&smiek2121_scripts
+#2️⃣true 或 false
 """
 cron: 20 10 */7 * *
 new Env('禁用重复任务');
