bfa73d8e99ef7357f7fd56303788b8124f890242
shidahuilang <81285256+shidahuilang@users.noreply.github.com>
2021-12-27 15:48:53 +0800

    Update lang1.sh

--- lang1.sh
+++ lang1.sh
@@ -101,8 +101,8 @@ function qinglong_port() {
   ;;
   esac
   done
-  export YUMING="请输入您当前服务器的IP[比如：192.168.2.99]"
-  echo
+  export local_ip="$(curl -sS --connect-timeout 10 -m 60 https://www.bt.cn/Api/getIpAddress)"
+  export YUMING="请输入您当前服务器的IP[比如：${local_ip}]"
   echo
   while :; do
   read -p " ${YUMING}：" IP
