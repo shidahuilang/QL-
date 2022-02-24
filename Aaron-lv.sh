78a25867f6e5b5e6554124749427c29e965dbb40
shidahuilang <81285256+shidahuilang@users.noreply.github.com>
2022-02-24 13:49:20 +0800

    Update Aaron-lv.sh

--- Aaron-lv.sh
+++ Aaron-lv.sh
@@ -101,7 +101,7 @@ if [ "$(grep -c wskey.py /ql/config/crontab.list)" = 0 ]; then
     echo
     # 获取token
     token=$(cat /ql/config/auth.json | jq --raw-output .token)
-    curl -s -H 'Accept: application/json' -H "Authorization: Bearer $token" -H 'Content-Type: application/json;charset=UTF-8' -H 'Accept-Language: zh-CN,zh;q=0.9' --data-binary '{"name":"每6小时转换WSKEY","command":"task wskey.py","schedule":"58 0-23/4 * * *"}' --compressed 'http://127.0.0.1:5700/api/crons?t=1633428022377'
+    curl -s -H 'Accept: application/json' -H "Authorization: Bearer $token" -H 'Content-Type: application/json;charset=UTF-8' -H 'Accept-Language: zh-CN,zh;q=0.9' --data-binary '{"name":"每4小时转换WSKEY","command":"task wskey.py","schedule":"58 0-23/4 * * *"}' --compressed 'http://127.0.0.1:5700/api/crons?t=1633428022377'
 fi
 sleep 1
 echo
