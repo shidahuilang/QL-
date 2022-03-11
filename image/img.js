7dc6f928db7d81066c05c7ac369e17cd496195db
shidahuilang <81285256+shidahuilang@users.noreply.github.com>
2022-03-12 01:12:02 +0800

    Update img.js

--- image/img.js
+++ image/img.js
@@ -3,3 +3,8 @@ docker exec -it qinglong bash -c "task /ql/jd/6-10.sh && python3 /ql/jd/6-10.py"
 运行命令后，显示  Please enter your phone (or bot token):
 就输入您的注册TG的电话，如果是大陆的格式就是+8613666666666
 然后TG接收到验证码填上，再下来，如果TG有密码就输入密码
+
+
+
+
+ python3 /opt/ql/scripts/jd_sms_login.py
