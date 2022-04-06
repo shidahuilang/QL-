74d7bf2ed8fbed54c62c44403c4c2b32aa92d49e
大灰狼 <81285256+shidahuilang@users.noreply.github.com>
2022-04-06 15:59:35 +0800

    Update disableDuplicateTasksImplement.py

--- feverrun/disableDuplicateTasksImplement.py
+++ feverrun/disableDuplicateTasksImplement.py
@@ -33,7 +33,7 @@ logger.addHandler(logging.StreamHandler())  # 添加控制台日志
 
 
 ip = "localhost"
-sub_str = os.getenv("RES_SUB", "smiek2121_scripts")
+sub_str = os.getenv("RES_SUB", "feverrun")
 sub_list = sub_str.split("&")
 res_only = os.getenv("RES_ONLY", True)
 headers = {
