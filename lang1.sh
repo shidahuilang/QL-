7612ed99cb09bd1cbfd67346c4dd7fead3c0404c
大灰狼 <81285256+shidahuilang@users.noreply.github.com>
2022-11-02 19:23:11 +0800

    青龙更换镜像2.11.3
    青龙更换镜像2.11.3

--- lang1.sh
+++ lang1.sh
@@ -356,7 +356,7 @@ docker run -dit \
   --name qinglong \
   --hostname qinglong \
   --restart unless-stopped \
-  whyour/qinglong:2.10.13
+  whyour/qinglong:2.11.3
   
   docker restart qinglong > /dev/null 2>&1
   sleep 2
